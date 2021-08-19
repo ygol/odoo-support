5cd9f2c3c1a08355dd24515e1f6471ebe0321abc
Yves Goldberg <admin@ygol.com>
2021-08-19 11:28:59 +0300

    [MIG] server_mode: Migration to 14.0

--- server_mode/models/fetchmail.py
+++ server_mode/models/fetchmail.py
@@ -6,7 +6,6 @@ from odoo.addons.server_mode.mode import get_mode
 class FetchmailServer(models.Model):
     _inherit = 'fetchmail.server'
 
-    @api.multi
     def button_confirm_login(self):
         if get_mode():
             raise UserError(_(
@@ -14,7 +13,6 @@ class FetchmailServer(models.Model):
                 get_mode()))
         return super(FetchmailServer, self).button_confirm_login()
 
-    @api.multi
     def fetch_mail(self):
         if get_mode():
             raise UserError(_(
@@ -22,7 +20,6 @@ class FetchmailServer(models.Model):
                 get_mode()))
         return super(FetchmailServer, self).fetch_mail()
 
-    @api.multi
     def connect(self):
         if get_mode():
             raise UserError(_(
