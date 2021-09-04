631752b1b7c83b5d168398945f57d557ead42e1e
Yves Goldberg <admin@ygol.com>
2021-09-04 13:03:37 +0300

    [MIG] adhoc_modules: Migration to 14.0

---
+++ adhoc_modules/models/module_dependency.py
@@ -0,0 +1,16 @@
+##############################################################################
+# For copyright and license notices, see __openerp__.py file in module root
+# directory
+##############################################################################
+from odoo import models, fields
+import logging
+
+_logger = logging.getLogger(__name__)
+
+
+class module_dependency(models.Model):
+    _inherit = "ir.module.module.dependency"
+
+    state = fields.Selection(
+        selection_add=[('ignored', 'Ignored')]
+    )
